fba1bf917c9730807d4708bbb4067699e6320f7b
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-25 19:52:33 -0300

    iniciando projeto web

---
+++ web/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="bg-white border-gray-200 dark:bg-emerald-500">
+    <div
+      class=" flex flex-wrap items-center justify-between mx-auto p-4 container"
+    >
+
+      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-emerald-500 md:dark:bg-emerald-500 dark:border-gray-700"
+        >
+          <li>
+            <a
+              href="/cadastro"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+
+              >Cadastro</a
+            >
+          </li>
+          <li>
+            <a
+              href="/"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >Listagem</a
+            >
+          </li>
+
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view></router-view>
+</template>
---
+++ web/src/views/CadastroEletro.vue
@@ -0,0 +1,54 @@
+<template>
+    <main class="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
+    <div class="container max-w-screen-lg mx-auto">
+      <div>
+        <div class="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
+          <div class="grid gap-4 gap-y-2 text-sm grid-cols-1">
+            <div>
+              <div class="grid gap-4 gap-y-5 text-sm grid-cols-1 auto-cols-max">
+                <div>
+                  <label for="nome">Nome</label>
+                  <input type="text" name="nome" id="nome" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  />
+                </div>
+  
+                <div >
+                  <label for="descricao">Descrição</label>
+                  <input type="text" name="descricao" id="descricao" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50" />
+                </div>
+  
+                <div>
+                  <label for="tensao">Tensão</label>
+                  <input type="text" name="tensao" id="tensao" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  />
+                </div>
+  
+                <div>
+                  <label for="marca">Marca</label>
+                  <input type="text" name="marca" id="marca" class="h-10 border mt-1 rounded px-4 w-full bg-gray-50"  />
+  
+                  <span class="text-gray-400 italic mt-2 block text-xs">Marcas aceitas: (Electrolux, Brastemp, Fischer, Samsung, LG)</span>
+                </div>
+  
+  
+  
+                <div class="text-right">
+                  <div class="inline-flex items-end">
+                    <button class="bg-emerald-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Cadastrar</button>
+                  </div>
+                </div>
+  
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  </template>
+  
+  <script>
+  export default {
+    name: "CadastroEletro",
+  
+  }
+  </script>
+  
