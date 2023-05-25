32ffdea55c1af013456c3641ad6c8a2334cc44ac
Matheus de Paula <matheusdepaula527@gmail.com>
2023-05-25 19:57:30 -0300

    criando rota de cadastro

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import CadastroEletro from '../views/CadastroEletro.vue'
 const routes = [
-
+  {
+    path: '/cadastro',
+    name: 'CadastroEletro',
+    component: CadastroEletro
+  },
 ]
 
 const router = createRouter({
